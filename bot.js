5299ef31b24b5ae90a23698456f40fe41e279461
rudyards <rudy@mightycheese.com>
2018-12-11 09:44:02 -0800

    basic functions

--- bot.js
+++ bot.js
@@ -20,8 +20,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 9) == 'Hey Janet') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[2];
+        var args = message.substring(9).split(' ');
+        // bot.sendMessage({
+        //     to: channelID,
+        //     message: args
+        // });
+        var cmd = args[1];
+        // bot.sendMessage({
+        //     to: channelID,
+        //     message: cmd
+        // });
 
 
         switch(cmd) {
@@ -31,6 +39,22 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Pong!'
                 });
+            break;
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Hi, I'm Janet. Anytime you say 'Hey Janet' in this server, I'll hear and try to help if I can.\
+                     \n'Hey Janet help' will get a list of features. \
+                     \n'Hey Janet tell me [search]' will get you a google result that I think is relevant."
+                });
+            break;
+            case 'tell':
+                args = args.slice(3)
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Searching for "+args.join(" ")+"\
+                    \nhttp://www.google.com/search?q="+args.join("%20")
+                })
             break;
             // Just add any case commands if you want to..
          }
